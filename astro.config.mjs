2bf131d3d73f9663d8fdf5367d93abf2400393c0
gfrabelo <gabrielrabelob@gmail.com>
2026-09-06 01:26:49 -0300

    feat: queime o barco

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://gfrabelo.github.io'
+  site: 'https://gabrielrabelo.dev'
 });
